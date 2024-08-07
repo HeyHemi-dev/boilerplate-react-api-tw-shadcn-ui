e74e866015ff8fdf8e41fa968084982c5cf15168
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-08-07 16:26:35 +1200

    Edit  tsconfig and viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,6 +8,14 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
+    },
+  },
+
+  //Required for Shadcn UI
+  //See https://ui.shadcn.com/docs/installation/vite
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./client"),
     },
   },
 })
